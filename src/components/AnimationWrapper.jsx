d9942bfc3be9bbc24e19e8f26481659ba72beeca
maxi famozo <famozomaximiliano@gmail.com>
2026-07-16 15:47:08 -0300

    probando ancho

--- src/components/AnimationWrapper.jsx
+++ src/components/AnimationWrapper.jsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 export default function AnimationWrapper({ children }) {
   return (
     <motion.div
+      className="w-full"
       initial={{ opacity: 0, y: 40 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.7 }}
